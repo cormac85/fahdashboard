431c15645f1f1ae9208e37b998b36a9dab598586
Cormac Nolan <cormacnolan85@gmail.com>
2020-04-25 21:54:29 +0100

    initial commit, just following tutorial

---
+++ ui.R
@@ -0,0 +1,16 @@
+library(shiny)
+fluidPage(
+  
+  titlePanel("Folding@Home Log Statistics Dashboard"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("numInput", "A numeric input:", value = 7, min = 1, max = 30)
+    ),
+    mainPanel(
+      p(strong("bold font "), em("italic font")),
+      p(code("code block")),
+      a(href="http://www.google.com", "link to Google")
+    )
+  )
+)
