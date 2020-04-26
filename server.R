48b9e9b98024ee8d1c4926b60aff79353ddcc7df
Cormac Nolan <cormacnolan85@gmail.com>
2020-04-27 00:00:50 +0100

    continuing tutorials...

--- server.R
+++ server.R
@@ -1,4 +1,30 @@
 library(shiny)
-function(input, output) {
-  
+library(fahlogstats)
+library(dplyr)
+
+# shiny_read_log <- function(file_path) { 
+#   
+#   suppressMessages(tibble::tibble(message = scan(file_path,
+#                                                  what = "character",
+#                                                  sep = "\r",
+#                                                  quiet = TRUE)))
+#   
+# }
+
+function(input, output, session) {
+  data <- reactive({
+    req(input$log_files)
+    
+    file_extension <- tools::file_ext(input$log_files$name)
+    switch(file_extension,
+           csv = read.csv(input$log_files$datapath, stringsAsFactors = FALSE),
+           validate(
+             "Invalid file. Please supply a .txt log file from FAH Client"
+           )
+    )
+  })
+  output$logs_df <- renderTable({
+    head(data())
+  })
 }
+
--- ui.R
+++ ui.R
@@ -1,16 +1,6 @@
 library(shiny)
 fluidPage(
-  
-  titlePanel("Folding@Home Log Statistics Dashboard"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      numericInput("numInput", "A numeric input:", value = 7, min = 1, max = 30)
-    ),
-    mainPanel(
-      p(strong("bold font "), em("italic font")),
-      p(code("code block")),
-      a(href="http://www.google.com", "link to Google")
-    )
-  )
+  fileInput("log_files", NULL, buttonLabel = "Upload:", multiple = TRUE,
+            accept = c(".txt")), 
+  tableOutput("logs_df")
 )
