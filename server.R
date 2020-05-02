3f694db5db651e6517b8b243112106f634e6bde4
Cormac Nolan <cormacnolan85@gmail.com>
2020-05-02 01:58:49 +0100

    Got simple credits plot working

--- server.R
+++ server.R
@@ -1,30 +1,61 @@
 library(shiny)
 library(fahlogstats)
 library(dplyr)
+library(lubridate)
+library(stringr)
 
-# shiny_read_log <- function(file_path) { 
-#   
-#   suppressMessages(tibble::tibble(message = scan(file_path,
-#                                                  what = "character",
-#                                                  sep = "\r",
-#                                                  quiet = TRUE)))
-#   
-# }
+shiny_read_log <- function(file_path) {
+
+  log_df <- suppressMessages(tibble::tibble(message = scan(file_path,
+                                                           what = "character",
+                                                           sep = "\r",
+                                                           quiet = TRUE)))
+  log_df <- dplyr::filter(log_df, !stringr::str_starts(message, "\\*"))
+  
+  log_start_date <-
+    log_df %>%
+    dplyr::filter(stringr::str_detect(message, " Log Started")) %>%
+    (function(x) x$message[1]) %>%
+    stringr::str_extract("[0-9]{4}-[0-9]{2}-[0-9]{2}") %>%
+    lubridate::ymd()
+  
+  log_df$log_date <- log_start_date
+  log_df$log_file_name <- "log.txt"
+  
+  log_df
+  
+}
 
 function(input, output, session) {
-  data <- reactive({
+  log_df <- reactive({
     req(input$log_files)
     
     file_extension <- tools::file_ext(input$log_files$name)
     switch(file_extension,
-           csv = read.csv(input$log_files$datapath, stringsAsFactors = FALSE),
+           txt = shiny_read_log(input$log_files$datapath),
            validate(
              "Invalid file. Please supply a .txt log file from FAH Client"
            )
     )
   })
-  output$logs_df <- renderTable({
-    head(data())
+  
+  work_unit_df <- reactive({
+    log_df() %>% 
+      fahlogstats::clean_logs() %>% 
+      fahlogstats::get_work_unit_data()
   })
+  
+  
+  
+  output$logs_df <- renderTable({
+    head(work_unit_df())
+  })
+  
+  output$credits_plot <- renderPlot({
+    work_unit_df() %>% 
+      get_credits() %>%
+      plot_credits(all_slots = TRUE)
+  })
+  
 }
 
--- ui.R
+++ ui.R
@@ -2,5 +2,6 @@ library(shiny)
 fluidPage(
   fileInput("log_files", NULL, buttonLabel = "Upload:", multiple = TRUE,
             accept = c(".txt")), 
-  tableOutput("logs_df")
+  tableOutput("logs_df"),
+  plotOutput("credits_plot")
 )
