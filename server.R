0ddea869ffe876710c8de79464755865172b3c2d
Cormac Nolan <cormacnolan85@gmail.com>
2020-05-11 00:30:18 +0100

    added mock ups of info & value boxes; plots now show live and some historical data

--- server.R
+++ server.R
@@ -6,6 +6,13 @@ library(stringr)
 library(shinydashboard)
 
 FAH_CLIENT_FOLDER_PATH <- file.path("~", "..", "AppData", "Roaming", "FAHClient")
+FAH_CLIENT_RECENT_LOGS_PATH <- file.path(FAH_CLIENT_FOLDER_PATH, "logs")
+FAH_CLIENT_LIVE_LOG_FILE_NAME <- "log.txt"
+NUMBER_LOGS_READ <- 8
+
+ggplot2::theme_set(
+  ggplot2::theme_minimal(base_size = 20)
+)
 
 shiny_read_log <- function(log_file_path) {
 
@@ -28,35 +35,65 @@ shiny_read_log <- function(log_file_path) {
     lubridate::ymd()
   
   log_df$log_date <- log_start_date
-  log_df$log_file_name <- "log.txt"
-  
+
   log_df
-  
 }
 
 function(input, output, session) {
-  log_df <- reactive({
-    shiny_read_log(file.path(FAH_CLIENT_FOLDER_PATH, "log.txt"))
+  live_log_df <- reactive({
+    live_log <- 
+      shiny_read_log(file.path(FAH_CLIENT_FOLDER_PATH, 
+                               FAH_CLIENT_LIVE_LOG_FILE_NAME)) %>% 
+      mutate(log_file_name = FAH_CLIENT_LIVE_LOG_FILE_NAME)
+    
+    live_log
+  })
+  
+  recent_logs_df <- reactive({
+    recent_logs <- 
+      tibble::tibble(
+        log_file_name = list.files(pattern = "*.txt",
+                                   path = FAH_CLIENT_RECENT_LOGS_PATH)
+      ) %>% 
+      arrange(log_file_name) %>% 
+      tail(NUMBER_LOGS_READ) %>% 
+      mutate(
+        log_file_path = file.path(FAH_CLIENT_RECENT_LOGS_PATH, log_file_name),
+        log_df = purrr::map(log_file_path, shiny_read_log)
+      ) %>% 
+      tidyr::unnest(log_df)
+    
+    print(recent_logs %>% pull(log_file_name) %>% unique())
+    recent_logs
+  })
+  
+  cleaned_logs <- reactive({
+    print(recent_logs_df())
+    print(live_log_df())
+    recent_logs_df() %>% 
+      fahlogstats::clean_logs() %>% 
+      dplyr::union_all(
+        fahlogstats::clean_logs(live_log_df())
+      )
   })
   
   work_unit_df <- reactive({
-    log_df() %>% 
-      fahlogstats::clean_logs() %>% 
+      cleaned_logs() %>% 
       fahlogstats::get_work_unit_data()
   })
   
   output$credits_plot <- renderPlot({
     work_unit_df() %>% 
       get_credits() %>%
-      plot_credits(all_slots = TRUE)
+      fahlogstats::plot_credits(all_slots = TRUE) 
   })
   
   output$network_plot <- renderPlot({
     work_unit_df() %>% 
       fahlogstats::get_network_usage() %>%
       fahlogstats::calculate_daily_network_usage() %>% 
-      plot_cumulative_network_usage()
-  })
+      fahlogstats::plot_cumulative_network_usage()
+  }, height = 700)
   
 }
 
--- ui.R
+++ ui.R
@@ -2,6 +2,9 @@ library(shiny)
 library(shinydashboard)
 
 OVERVIEW_PLOT_WIDTH = 9
+OVERVIEW_PLOT_HEIGHT = 9
+CREDITS_ICON = "award"
+NETWORK_ICON = "wifi"
 
 shinydashboard::dashboardPage(
   shinydashboard::dashboardHeader(title = "Folding@Home Live Stats"),
@@ -16,16 +19,39 @@ shinydashboard::dashboardPage(
     tabItems(
       tabItem(tabName = "overview",
         fluidRow(
+          shinydashboard::infoBox(title = "Date Range", 
+                                  value = "2010/01/01 \U2192 2010/02/01",
+                                  width = 12,
+                                  icon = icon("calendar-alt"))
+        ),
+        fluidRow(
           shinydashboard::box(plotOutput("credits_plot"), 
                               width = OVERVIEW_PLOT_WIDTH,
                               title = "Credits", status = "primary",
-                              solidHeader = TRUE)
+                              solidHeader = TRUE),
+          shinydashboard::valueBox(1000000, "Total Credits",
+                                   width = 3, 
+                                   color = "light-blue",
+                                   icon = icon(CREDITS_ICON)),
+          shinydashboard::valueBox(10000, "Credits per Day",
+                                   width = 3,
+                                   color = "aqua",
+                                   icon = icon(CREDITS_ICON))
         ),
         fluidRow(
           shinydashboard::box(plotOutput("network_plot"), 
                               width = OVERVIEW_PLOT_WIDTH,
-                              title = "Network", status = "primary",
-                              solidHeader = TRUE)
+                              title = "Network", status = "warning",
+                              solidHeader = TRUE,
+                              height = 800),
+          shinydashboard::valueBox(3.1, "Total Usage (GiB)",
+                                   width = 3, 
+                                   color = "yellow",
+                                   icon = icon(NETWORK_ICON)),
+          shinydashboard::valueBox(0.5, "Usage per Day (GiB)",
+                                   width = 3, 
+                                   color = "orange",
+                                   icon = icon(NETWORK_ICON))
           
         )
       ),
