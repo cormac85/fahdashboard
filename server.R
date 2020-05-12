1bb112a18e37c7e6c804648f1774cd41e4eab31f
Cormac Nolan <cormacnolan85@gmail.com>
2020-05-12 09:45:36 +0100

    split credits and network charts into separate tabs

--- server.R
+++ server.R
@@ -4,16 +4,19 @@ library(dplyr)
 library(lubridate)
 library(stringr)
 library(shinydashboard)
+library(scales)
 
+# Logs
 FAH_CLIENT_FOLDER_PATH <- file.path("~", "..", "AppData", "Roaming", "FAHClient")
 FAH_CLIENT_RECENT_LOGS_PATH <- file.path(FAH_CLIENT_FOLDER_PATH, "logs")
 FAH_CLIENT_LIVE_LOG_FILE_NAME <- "log.txt"
 NUMBER_LOGS_READ <- 8
+RENDER_PERIOD_MINUTES <- 5
+# Styling
+CREDITS_ICON = "award"
+NETWORK_ICON = "wifi"
 
-ggplot2::theme_set(
-  ggplot2::theme_minimal(base_size = 20)
-)
-
+# Functions
 shiny_read_log <- function(log_file_path) {
 
   log_df <- suppressMessages(
@@ -40,7 +43,14 @@ shiny_read_log <- function(log_file_path) {
 }
 
 function(input, output, session) {
+  
+  global_timer <- reactiveTimer(1000 * 60 * RENDER_PERIOD_MINUTES)
+  
+  #############
+  # Read Logs #
+  #############
   live_log_df <- reactive({
+    global_timer()
     live_log <- 
       shiny_read_log(file.path(FAH_CLIENT_FOLDER_PATH, 
                                FAH_CLIENT_LIVE_LOG_FILE_NAME)) %>% 
@@ -50,6 +60,7 @@ function(input, output, session) {
   })
   
   recent_logs_df <- reactive({
+    global_timer()
     recent_logs <- 
       tibble::tibble(
         log_file_name = list.files(pattern = "*.txt",
@@ -68,6 +79,8 @@ function(input, output, session) {
   })
   
   cleaned_logs <- reactive({
+    global_timer()
+    
     print(recent_logs_df())
     print(live_log_df())
     recent_logs_df() %>% 
@@ -78,22 +91,139 @@ function(input, output, session) {
   })
   
   work_unit_df <- reactive({
-      cleaned_logs() %>% 
+    global_timer()
+    
+    cleaned_logs() %>% 
       fahlogstats::get_work_unit_data()
   })
   
+  ###########
+  # Credits #
+  ###########
+  credits_df <- reactive({
+    global_timer()
+    
+    credits <-
+      work_unit_df() %>% 
+      get_credits()
+    print(credits)
+    
+    credits
+  })
+  
+  credits_per_day <- reactive({
+    global_timer()
+    
+    credits_df() %>% 
+      dplyr::group_by(log_date) %>% 
+      dplyr::summarise(credits_per_day = sum(credits_attributed))
+  })
+  
+  ###########
+  # Network #
+  ###########
+  daily_network_usage_df <- reactive({
+    global_timer()
+    
+    usage <- 
+      work_unit_df() %>% 
+      fahlogstats::get_network_usage() %>% 
+      fahlogstats::calculate_daily_network_usage()
+    print(usage)
+    usage
+  })
+  
+  total_daily_network_usage_df <- reactive({
+    global_timer()
+    
+    daily_network_usage_df() %>% 
+      dplyr::group_by(log_date) %>% 
+      dplyr::summarise(total_usage_mib = sum(total_usage_mib))
+  })
+  
+  #########
+  # Plots #
+  #########
   output$credits_plot <- renderPlot({
-    work_unit_df() %>% 
-      get_credits() %>%
+    credits_df() %>% 
       fahlogstats::plot_credits(all_slots = TRUE) 
   })
   
   output$network_plot <- renderPlot({
-    work_unit_df() %>% 
-      fahlogstats::get_network_usage() %>%
-      fahlogstats::calculate_daily_network_usage() %>% 
+    daily_network_usage_df() %>% 
       fahlogstats::plot_cumulative_network_usage()
-  }, height = 700)
+  }, height = 600)
   
+  ###############
+  # Info Boxes #
+  ###############
+  date_range_box <- reactive({
+    log_start_date <- min(cleaned_logs()$log_date)
+    log_end_date <- max(cleaned_logs()$log_date)
+    
+    infoBox(title = "Date Range", 
+            value = paste0(
+              strftime(log_start_date, format = "%A %Y-%m-%d"),
+              " \U2192 ",
+              strftime(log_end_date, format = "%A %Y-%m-%d")
+            ),
+            width = 12,
+            icon = icon("calendar-alt"),
+            subtitle = paste0(log_end_date - log_start_date,
+                              " days"))
+  })
+  
+  output$date_range_box_credits <- renderUI({
+    date_range_box()
+  })
+  
+  output$date_range_box_network <- renderUI({
+    date_range_box()
+  })
+  
+  ###############
+  # Value Boxes #
+  ###############
+  output$total_credits_box <- renderUI({
+    valueBox(
+      subtitle = shiny::p("Total Credits"),
+      value = scales::comma(sum(credits_df()$credits_attributed)),
+      icon = icon(CREDITS_ICON),
+      color = "light-blue",
+      width = 3
+    )
+  })
+  output$credits_per_day_box <- renderUI({
+    valueBox(
+      subtitle = shiny::p("Credits per Day"),
+      value = scales::comma(mean(credits_per_day()$credits_per_day)),
+      icon = icon(CREDITS_ICON),
+      color = "aqua",
+      width = 3
+    )
+  })
+  
+  output$total_network_usage_box <- renderUI({
+    valueBox(
+      subtitle = shiny::p("Total Usage (MiB) "),
+      value = scales::comma(
+        sum(total_daily_network_usage_df()$total_usage_mib)
+        ),
+      icon = icon(NETWORK_ICON),
+      color = "yellow",
+      width = 3
+    )
+  })
+  output$network_usage_per_day_box <- renderUI({
+    valueBox(
+      subtitle = shiny::p("Usage per Day (MiB)"),
+      value = scales::comma(
+        mean(total_daily_network_usage_df()$total_usage_mib)
+        ),
+      icon = icon(NETWORK_ICON),
+      color = "orange",
+      width = 3
+    )
+  })
 }
 
--- ui.R
+++ ui.R
@@ -1,62 +1,54 @@
 library(shiny)
 library(shinydashboard)
 
-OVERVIEW_PLOT_WIDTH = 9
-OVERVIEW_PLOT_HEIGHT = 9
+# Styling
 CREDITS_ICON = "award"
 NETWORK_ICON = "wifi"
+LIVE_ICON = "heartbeat"
+OVERVIEW_PLOT_WIDTH = 9
+OVERVIEW_PLOT_HEIGHT = 9
 
 shinydashboard::dashboardPage(
   shinydashboard::dashboardHeader(title = "Folding@Home Live Stats"),
   shinydashboard::dashboardSidebar(
     sidebarMenu(
-      menuItem("Overview", tabName = "overview", icon = icon("book-reader")),
-      menuItem("Live", tabName = "live",  icon = icon("heartbeat"),
+      menuItem("Credits", tabName = "credits", icon = icon(CREDITS_ICON)),
+      menuItem("Network", tabName = "network", icon = icon(NETWORK_ICON)),
+      menuItem("Live", tabName = "live",  icon = icon(LIVE_ICON),
                badgeLabel = "\U1F528", badgeColor = "yellow")
     )
   ),
   shinydashboard::dashboardBody(
     tabItems(
-      tabItem(tabName = "overview",
-        fluidRow(
-          shinydashboard::infoBox(title = "Date Range", 
-                                  value = "2010/01/01 \U2192 2010/02/01",
-                                  width = 12,
-                                  icon = icon("calendar-alt"))
-        ),
-        fluidRow(
-          shinydashboard::box(plotOutput("credits_plot"), 
-                              width = OVERVIEW_PLOT_WIDTH,
-                              title = "Credits", status = "primary",
-                              solidHeader = TRUE),
-          shinydashboard::valueBox(1000000, "Total Credits",
-                                   width = 3, 
-                                   color = "light-blue",
-                                   icon = icon(CREDITS_ICON)),
-          shinydashboard::valueBox(10000, "Credits per Day",
-                                   width = 3,
-                                   color = "aqua",
-                                   icon = icon(CREDITS_ICON))
-        ),
-        fluidRow(
-          shinydashboard::box(plotOutput("network_plot"), 
-                              width = OVERVIEW_PLOT_WIDTH,
-                              title = "Network", status = "warning",
-                              solidHeader = TRUE,
-                              height = 800),
-          shinydashboard::valueBox(3.1, "Total Usage (GiB)",
-                                   width = 3, 
-                                   color = "yellow",
-                                   icon = icon(NETWORK_ICON)),
-          shinydashboard::valueBox(0.5, "Usage per Day (GiB)",
-                                   width = 3, 
-                                   color = "orange",
-                                   icon = icon(NETWORK_ICON))
-          
-        )
+      tabItem(tabName = "credits",
+              fluidRow(
+                uiOutput("date_range_box_credits")
+              ),
+              fluidRow(
+                shinydashboard::box(plotOutput("credits_plot"), 
+                                    width = OVERVIEW_PLOT_WIDTH,
+                                    title = "Credits", status = "primary",
+                                    solidHeader = TRUE),
+                uiOutput("total_credits_box"),
+                uiOutput("credits_per_day_box")
+              )
+      ),
+      tabItem(tabName = "network",
+              fluidRow(
+                uiOutput("date_range_box_network")
+              ),
+              fluidRow(
+                shinydashboard::box(plotOutput("network_plot"), 
+                                    width = OVERVIEW_PLOT_WIDTH,
+                                    title = "Network", status = "warning",
+                                    solidHeader = TRUE,
+                                    height = 700),
+                uiOutput("total_network_usage_box"),
+                uiOutput("network_usage_per_day_box")
+              )
       ),
       tabItem(tabName = "live",
-        shiny::h3("Under Construction!")
+              shiny::h3("Under Construction!")
       )
     )
   )
